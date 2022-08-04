b60b0f2d8e9fda927cf7a7ca193e8d342e14e5c8
ricardojo619 <ricardojonathan619@gmail.com>
2022-08-04 22:55:22 +0700

    perubahan halaman berita

--- resources/views/index/body.blade.php
+++ resources/views/index/body.blade.php
@@ -80,9 +80,10 @@
 
     <hr class="featurette-divider">
     
-    <h1 class="text-center mb-4 text-underline" style="text-decoration: underline;">
+    <h1 class="text-center mb-4">
       Gallery
   </h1>
+  <hr class="hr-bold mx-auto">
     <div class="row featurette">
       <div class="col-md-7">
         <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
@@ -118,43 +119,39 @@
         <span class="text-coulumn" style="margin-top: 12px">INFO LAIN</span>
         <hr class="mx-auto" width="60%">
         <div class="text-start font-poppin">Tgl Bln Thn</div>
-        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#">Pengadaan CPNS 2022</a></div><br>
+        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#" style="color:rgb(255, 255, 255)">Pengadaan CPNS 2022</a></div><br>
         <div class="text-start font-poppin">Tgl Bln Thn</div>
-        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#">Pengadaan CPNS 2022</a></div><br>
+        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#" style="color:rgb(255, 255, 255)">Pengadaan CPNS 2022</a></div><br>
         <div class="text-start font-poppin">Tgl Bln Thn</div>
-        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#">Pengadaan CPNS 2022</a></div><br>
+        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#" style="color:rgb(255, 255, 255)">Pengadaan CPNS 2022</a></div><br>
         <br>
-         <div class="text-center">
-            <a href="#" class="btn btn-light mb-4 mt-2 justify-content-center">LIHAT SEMUA</a>
-          </div>
       </div>
       <div class="col" style="background-color: rgba(226, 223, 223, 0.825);color:rgb(0, 0, 0));"><br>
         <span class="text-coulumn" style="margin-top: 12px">EVENT</span>
         <hr class="mx-auto" width="60%">
         <div class="text-start font-poppin">Tgl Bln Thn</div>
-        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#">Sosialisasi SIMPEG & SAPK 2022</a></div><br>
+        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#" style="color:black">Sosialisasi SIMPEG & SAPK 2022</a></div><br>
         <div class="text-start font-poppin">Tgl Bln Thn</div>
-        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#">Sosialisasi SIMPEG & SAPK 2022</a></div><br>
+        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#" style="color:black">Sosialisasi SIMPEG & SAPK 2022</a></div><br>
         <div class="text-start font-poppin">Tgl Bln Thn</div>
-        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#">Sosialisasi SIMPEG & SAPK 2022</a></div><br>
+        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#" style="color:black">Sosialisasi SIMPEG & SAPK 2022</a></div><br>
         <br>
          <div class="text-center">
-            <a href="#" class="btn btn-secondary mb-4 mt-2 justify-content-center">LIHAT SEMUA</a>
+          <a href="#" class="btn btn-dark mb-4 mt-2 justify-content-center" style="border-radius: 30px">
+            <span style="color: rgba(226, 223, 223, 0.825);">LIHAT SEMUA</span>
+        </a>
           </div>
       </div>
       <div class="col" style="background-color: rgba(2, 83, 107, 0.811);color: rgb(255, 255, 255);"><br>
         <span class="text-coulumn" style="margin-top: 12px">PENGUMUMAN</span>
         <hr class="mx-auto" width="60%">
         <div class="text-start font-poppin">Tgl Bln Thn</div>
-        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#">Bimbingan Teknis Pengelola Adm</a></div><br>
+        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#" style="color:rgb(255, 255, 255)">Bimbingan Teknis Pengelola Adm</a></div><br>
         <div class="text-start font-poppin">Tgl Bln Thn</div>
-        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#">Bimbingan Teknis Pengelola Adm</a></div><br>
+        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#" style="color:rgb(255, 255, 255)">Bimbingan Teknis Pengelola Adm</a></div><br>
         <div class="text-start font-poppin">Tgl Bln Thn</div>
-        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#">Bimbingan Teknis Pengelola Adm</a></div><br>
+        <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="#" style="color:rgb(255, 255, 255)">Bimbingan Teknis Pengelola Adm</a></div><br>
         <br>
-         <div class="text-center">
-            <a href="#" class="btn btn-light mb-4 mt-2 justify-content-center">LIHAT SEMUA</a>
-          </div>
       </div>
     </div>
   </div>
