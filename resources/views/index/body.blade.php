9897659c635ac426245e11baad8e1f99ace8c87c
ricardojo619 <ricardojonathan619@gmail.com>
2022-08-05 18:48:38 +0700

    Perubahan Gallery v2 (lgsg pull ya)

--- resources/views/index/body.blade.php
+++ resources/views/index/body.blade.php
@@ -145,7 +145,7 @@
         <h2>Serba Serbi</h2>
       </div>
       <div class="text-center">
-        <a href="#" class="btn btn-danger mb-4 mt-2 justify-content-center">Struktur Organisasi</a>
+        <a href="#" class="btn btn-info mb-4 mt-2 justify-content-center">Struktur Organisasi</a>
       </div>
     </div>
   </div>
