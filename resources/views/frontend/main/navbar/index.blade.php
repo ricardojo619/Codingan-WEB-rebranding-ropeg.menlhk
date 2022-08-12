50eb554e8c1ac18ce7553e1c9ea1290468fdb99a
ricardojo619 <ricardojonathan619@gmail.com>
2022-08-12 16:28:04 +0700

    Membuat route Navbar

--- resources/views/frontend/main/navbar/index.blade.php
+++ resources/views/frontend/main/navbar/index.blade.php
@@ -28,10 +28,10 @@
               <a class="nav-link" aria-current="page" href="#">Beranda</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Profil</a>
+              <a class="nav-link" href="/Profil">Profil</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Berita</a>
+              <a class="nav-link" href="/Berita">Berita</a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
@@ -39,18 +39,18 @@
                   Administrasi Kepegawaian
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown"> 
-                  <li><a class="dropdown-item" href="#">Pengangkatan PNS</a></li>
-                  <li><a class="dropdown-item" href="#">Kartu Pegawai</a></li>
-                  <li><a class="dropdown-item" href="#">Kartu Istri/Suami</a></li>
+                  <li><a class="dropdown-item" href="/Pengpns">Pengangkatan PNS</a></li>
+                  <li><a class="dropdown-item" href="/Karpeg">Kartu Pegawai</a></li>
+                  <li><a class="dropdown-item" href="/Karissu">Kartu Istri/Suami</a></li>
                   <li class="nav-item dropdown">
                           <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown"
                               aria-haspopup="true" aria-expanded="false">
                               Alih Tugas
                           </a>
                           <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                              <li><a class="dropdown-item" href="#">Alih Tugas Lingkungan KLHK</a></li>
-                              <li><a class="dropdown-item" href="#">Alih Tugas dari Kementerian LHK ke PEMDA/Instansi Lain</a></li>
-                              <li><a class="dropdown-item" href="#">Alih Tugas dari PEMDA/Instansi Lain ke Lingkup Kementerian LHK</a></li>  
+                              <li><a class="dropdown-item" href="/Altuglk">Alih Tugas Lingkungan KLHK</a></li>
+                              <li><a class="dropdown-item" href="Altugpemdal">Alih Tugas dari Kementerian LHK ke PEMDA/Instansi Lain</a></li>
+                              <li><a class="dropdown-item" href="Altugpemdalk">Alih Tugas dari PEMDA/Instansi Lain ke Lingkup Kementerian LHK</a></li>  
                           </ul>
                   </li>
                   <li class="nav-item dropdown">
@@ -59,24 +59,24 @@
                         Pengembangan Pegawai
                     </a> 
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">  
-                        <li><a href="#" class="dropdown-item">Ujian Dinas</a></li>
-                        <li><a href="#" class="dropdown-item">Ujian GI/PI/PG</a></li>
-                        <li><a href="#" class="dropdown-item">Izin Belajar/Tugas Belajar</a></li>
-                        <li><a href="#" class="dropdown-item">Seleksi 4 Kriteria</a></li>
+                        <li><a href="/Ujdin" class="dropdown-item">Ujian Dinas</a></li>
+                        <li><a href="/Ujgi" class="dropdown-item">Ujian GI/PI/PG</a></li>
+                        <li><a href="/Izbel" class="dropdown-item">Izin Belajar/Tugas Belajar</a></li>
+                        <li><a href="/Sel4kri" class="dropdown-item">Seleksi 4 Kriteria</a></li>
                         <li class="nav-item dropdown">
                             <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false">
                                 Diklat Kepemimpinan
                             </a>
                             <ul class="dropdown-menu left" aria-labelledby="navbarDropdown2">
-                                <li><a class="dropdown-item" href="#">Tingkat I</a></li>
-                                <li><a class="dropdown-item" href="#">Tingkat II</a></li> 
-                                <li><a class="dropdown-item" href="#">Tingkat III</a></li> 
-                                <li><a class="dropdown-item" href="#">Tingkat IV</a></li> 
+                                <li><a class="dropdown-item" href="/T1">Tingkat I</a></li>
+                                <li><a class="dropdown-item" href="/T2">Tingkat II</a></li> 
+                                <li><a class="dropdown-item" href="/T3">Tingkat III</a></li> 
+                                <li><a class="dropdown-item" href="/T4">Tingkat IV</a></li> 
                             </ul>
                         </li>
-                        <li><a href="#" class="dropdown-item">Personal Assesment Centre (PAC)</a></li>
-                        <li><a href="#" class="dropdown-item">Seleksi Jabatan Lowong Pimpinan Tinggi</a></li>
+                        <li><a href="/PAC" class="dropdown-item">Personal Assesment Centre (PAC)</a></li>
+                        <li><a href="/Seljalo" class="dropdown-item">Seleksi Jabatan Lowong Pimpinan Tinggi</a></li>
                     </ul>
                   </li>
 
@@ -86,10 +86,10 @@
                         LHKPN
                     </a> 
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">  
-                      <a href="#" class="dropdown-item">Mengenai LHKPN</a>
-                      <a href="#" class="dropdown-item">Prosedur Pelayanan LHKPN</a> 
-                      <a href="#" class="dropdown-item">Bimbingan Teknis Pengisian</a>
-                      <a href="#" class="dropdown-item">Formulir LHKPN</a> 
+                      <a href="/Menglhkpn" class="dropdown-item">Mengenai LHKPN</a>
+                      <a href="/Prolhkpn" class="dropdown-item">Prosedur Pelayanan LHKPN</a> 
+                      <a href="/Bimtek" class="dropdown-item">Bimbingan Teknis Pengisian</a>
+                      <a href="/Forlhkpn" class="dropdown-item">Formulir LHKPN</a> 
                     </ul>
                   </li>
 
@@ -99,10 +99,10 @@
                         Pensiun
                     </a> 
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">  
-                      <a href="#" class="dropdown-item">Pensiun Otomatis (Mencapai Batas Usia Pensiun)</a>
-                      <a href="#" class="dropdown-item">Pensiun Janda/Duda dari PNS yang Tewas</a> 
-                      <a href="#" class="dropdown-item">Pensiun Janda/Duda dari PNS yang Meninggal Dunia</a>
-                      <a href="#" class="dropdown-item">Pensiun Janda/Duda dari Pensiunan PNS YANG Meninggal Dunia</a>
+                      <a href="/Penot" class="dropdown-item">Pensiun Otomatis (Mencapai Batas Usia Pensiun)</a>
+                      <a href="/Penjadute" class="dropdown-item">Pensiun Janda/Duda dari PNS yang Tewas</a> 
+                      <a href="/Penjadumen" class="dropdown-item">Pensiun Janda/Duda dari PNS yang Meninggal Dunia</a>
+                      <a href="/Penjadupemen" class="dropdown-item">Pensiun Janda/Duda dari Pensiunan PNS YANG Meninggal Dunia</a>
                     </ul>
                   </li>
 
@@ -112,14 +112,14 @@
                         Satya Lencana
                     </a> 
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">  
-                        <a href="#" class="dropdown-item">Bintang Jasa</a>
-                        <a href="#" class="dropdown-item">Satyalancana Pembangunan</a> 
-                        <a href="#" class="dropdown-item">Satyalancana Karya Satya</a>
-                        <a href="#" class="dropdown-item">Satyalancana Wirakarya</a> 
+                        <a href="/Binjas" class="dropdown-item">Bintang Jasa</a>
+                        <a href="/Satlenpem" class="dropdown-item">Satyalancana Pembangunan</a> 
+                        <a href="/Satlenkarsa" class="dropdown-item">Satyalancana Karya Satya</a>
+                        <a href="/Satlenwira" class="dropdown-item">Satyalancana Wirakarya</a> 
                     </ul>
                   </li>
-                  <li><a class="dropdown-item" href="#">Pencantuman Gelar</a></li>
-                  <li><a class="dropdown-item" href="#">Ralat NIP</a></li>
+                  <li><a class="dropdown-item" href="/Pengel">Pencantuman Gelar</a></li>
+                  <li><a class="dropdown-item" href="/Ralnamnip">Ralat NIP</a></li>
                    
               </ul>
             </li>  
@@ -134,14 +134,14 @@
                         Kenaikan Pangkat
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                        <li><a class="dropdown-item" href="#">Kenaikan Pangkat Reguler</a></li>
-                        <li><a class="dropdown-item" href="#">Kenaikan Pangkat Struktural</a></li>
-                        <li><a class="dropdown-item" href="#">Kenaikan Pangkat Fungsional</a></li>
-                        <li><a class="dropdown-item" href="#">Kenaikan Pangkat Penyesuaian Ijazah</a></li>  
+                        <li><a class="dropdown-item" href="/Kenpangre">Kenaikan Pangkat Reguler</a></li>
+                        <li><a class="dropdown-item" href="/Kenpangstru">Kenaikan Pangkat Struktural</a></li>
+                        <li><a class="dropdown-item" href="/Kenpangfu">Kenaikan Pangkat Fungsional</a></li>
+                        <li><a class="dropdown-item" href="/Kenpangpe">Kenaikan Pangkat Penyesuaian Ijazah</a></li>  
                     </ul>
 
                   </li>
-                  <li><a class="dropdown-item" href="#">Alih Tugas</a></li>
+                  <li><a class="dropdown-item" href="/Altugk">Alih Tugas</a></li>
 
 
                   <li class="nav-item dropdown">
@@ -150,11 +150,11 @@
                         Pengembangan Pegawai
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                        <li><a class="dropdown-item" href="#">Ujian GI/PI/PG</a></li>
-                        <li><a class="dropdown-item" href="#">Izin Belajar/Tugas Belajar</a></li>
-                        <li><a class="dropdown-item" href="#">Seleksi 4 Kriteria</a></li>
-                        <li><a class="dropdown-item" href="#">Diklat Kepemimpinan</a></li>  
-                        <li><a class="dropdown-item" href="#">PAC</a></li>  
+                        <li><a class="dropdown-item" href="/Ujgipi">Ujian GI/PI/PG</a></li>
+                        <li><a class="dropdown-item" href="/Izbell">Izin Belajar/Tugas Belajar</a></li>
+                        <li><a class="dropdown-item" href="/Sel4krii">Seleksi 4 Kriteria</a></li>
+                        <li><a class="dropdown-item" href="/Dikpem">Diklat Kepemimpinan</a></li>  
+                        <li><a class="dropdown-item" href="/Pengempac">PAC</a></li>  
                     </ul>
 
                   </li>
@@ -165,19 +165,19 @@
                         LHKPN
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                        <li><a class="dropdown-item" href="#">Verifikasi KPK</a></li> 
+                        <li><a class="dropdown-item" href="/Verkpk">Verifikasi KPK</a></li> 
                     </ul>
 
                   </li> 
 
                   
-                  <li><a class="dropdown-item" href="#">Satya Lencana</a></li>
-                  <li><a class="dropdown-item" href="#">Inpassing</a></li>
-                  <li><a class="dropdown-item" href="#">Penempatan Kembali Karyasiswa</a></li>
+                  <li><a class="dropdown-item" href="/Satlencana">Satya Lencana</a></li>
+                  <li><a class="dropdown-item" href="/Inpassing">Inpassing</a></li>
+                  <li><a class="dropdown-item" href="/Penkemkar">Penempatan Kembali Karyasiswa</a></li>
                 </ul>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Regulasi</a>
+              <a class="nav-link" href="/Regulasi">Regulasi</a>
             </li>
           
             
@@ -188,9 +188,9 @@
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown"> 
                 
-                <li><a class="dropdown-item" href="#">BUP</a></li> 
-                <li><a class="dropdown-item" href="#">KPO</a></li> 
-                <li><a class="dropdown-item" href="#">Struktural</a></li> 
+                <li><a class="dropdown-item" href="/BUP">BUP</a></li> 
+                <li><a class="dropdown-item" href="/KPO">KPO</a></li> 
+                <li><a class="dropdown-item" href="/Struktural">Struktural</a></li> 
               </ul>
           </li>
           </ul>
