1674641d50ecb63aab5cb6d9631a0725c016b5f5
Deva Dwi Satrio <devadwisatrio6@gmail.com>
2022-08-15 14:31:58 +0700

    Positioning informasi terbaru index

--- resources/views/frontend/main/info/index.blade.php
+++ resources/views/frontend/main/info/index.blade.php
@@ -2,28 +2,41 @@
     Informasi Terbaru
   </h2>
   <hr class="hr-bold mx-auto">
-<div class="container marketing">
-<div class="row">
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+<div class="container">
+  <div class="card-group row">
 
-        <h2>Seleksi Terbuka Pengisian JPT di Lingkungan Sekretariat Jendral DPR RI</h2>
-        <p>Kosong.</p>
-        <p><a class="btn btn-secondary" href="#">Selengkapnya &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+    <div class="col-lg-4 d-flex align-items-stretch">
+      <div class="card" style="width: 27rem;">
+        <img class="card-img-top" src="{{ asset('img/imgcap.png')}}" class="card-img-top" alt="...">
+        <div class="card-body d-flex flex-column">
+          <h5 class="card-title mt-4" style="font-weight:700;text-align: center;">Seleksi Terbuka Pengisian JPT di Lingkungan Sekretariat Jendral DPR RI</h5>
+          <p class="card-text mt-auto">Kosong.</p>
+          <a class="mt-auto mx-auto btn btn-secondary" href="#">Selengkapnya &raquo;</a>
+        </div>
+      </div>
+    </div>
 
-        <h2>Pengumuman SKD CPNS 2022</h2>
-        <p>Kosong.</p>
-        <p><a class="btn btn-secondary" href="#">Selengkapnya &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+    <div class="col-lg-4 d-flex align-items-stretch">
+      <div class="card" style="width: 27rem;">
+        <img class="card-img-top" src="{{ asset('img/imgcap.png')}}" class="card-img-top" alt="...">
+        <div class="card-body d-flex flex-column">
+            <h5 class="card-title mt-4" style="font-weight:700;text-align: center;">Pengumuman SKD CPNS 2022</h5>
+            <p class="card-text mt-auto">Kosong.</p>
+            <a class="mt-auto mx-auto btn btn-secondary" href="#">Selengkapnya &raquo;</a>
+        </div>
+      </div>
+    </div>
+      
 
-        <h2>Sosialisasi SIMPEG dan MySAPK 2022</h2>
-        <p>kosong.</p>
-        <p><a class="btn btn-secondary" href="#">Selengkapnya &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->
-    </div><!-- /.row -->
+    <div class="col-lg-4 d-flex align-items-stretch">
+      <div class="card" style="width: 27rem;">
+        <img class="card-img-top" src="{{ asset('img/imgcap.png')}}" class="card-img-top" alt="...">
+        <div class="card-body d-flex flex-column">
+          <h5 class="card-title mt-4" style="font-weight:700;text-align: center;">Sosialisasi SIMPEG dan MySAPK 2022</h5>
+          <p class="card-text mt-auto">Kosong.</p>
+          <a class="mt-auto mx-auto btn btn-secondary" href="#">Selengkapnya &raquo;</a>
+        </div>
+      </div>
+  </div>
+  
 </div>
