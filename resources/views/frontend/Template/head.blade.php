6411cb9a2f5f851c36c0c2ff29c2a6bebf6ea8bd
Muhammad Naufal Rafi Pratama <107117812+MuhammadNaufalRafi2605@users.noreply.github.com>
2022-08-25 08:43:32 +0700

    Penggantian warna header dan footer, perubahan tulisan bagian info, penggabungan bagian pengumuman dan info terbaru, perapihan running text

--- resources/views/frontend/Template/head.blade.php
+++ resources/views/frontend/Template/head.blade.php
@@ -8,6 +8,10 @@
     <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
     <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
     <style>
+      /* #001219 */
+      .navbar-nav .nav-link {
+    color: #f5f9f6 !important ;
+}
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -23,6 +27,50 @@
       }
     </style>
 
+<style>
+      .p_font { 
+    font-size: 16px;
+    overflow: hidden;
+    line-height: normal;
+    text-overflow: ellipsis;
+    white-space: normal;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    padding: 0 10px 5px 10px; 
+  }.p_font{
+    display: -webkit-box;
+  }
+    .bg-navbarr {
+      background-color: #005f73;
+      
+  }
+  .bg-darkku{
+      background-color: #005f73;
+      font-size: 14px;
+  }.round {
+      border-radius: 1.05rem!important;
+  }
+  .title-linekita {
+      position: relative; /* important for absolute child to work */
+      font-size: 18px;
+      font-weight: normal;
+      padding: 0 0 4px;
+      text-align: left;
+  }
+  H5{
+      margin-left: 80px;
+  }
+
+  .title-linekita::after {
+      content: ''; /* required to display pseudo elements */
+      height: 1px; /* this works like a border-width */
+      width: 10%; /* you can use a percentage of parent or fixed px value */
+      background: #CCC; /* the color of border */
+      position: absolute;
+      bottom: 0; /* position it at the bottom of parent */
+      margin: 0 auto; left: 0;  /* horizontal centering */
+  }
+</style>
 
     <!-- Custom styles for this template -->
     <link href="{{ asset('carousel/carousel.css') }}" rel="stylesheet">
