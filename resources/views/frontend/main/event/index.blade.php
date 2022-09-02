20383e1794ad507c75be457b7197e210e1de7a8d
itsnotyourdev <devadwisatrio6@gmail.com>
2022-09-02 16:18:38 +0700

    Link index event, pengumuman, info lain

--- resources/views/frontend/main/event/index.blade.php
+++ resources/views/frontend/main/event/index.blade.php
@@ -4,31 +4,61 @@
       <div class="col" style="background-color: rgba(226, 223, 223, 0.825);color: black;"><br>
         <span class="text-coulumn" style="margin-top: 12px">INFO LAIN</span>
         <hr class="mx-auto" width="40%">
-        <div class="position-relative"> 
+        <div class="position-relative">
           <div class="position-absolute top-0 start-25">
-            <div class="text-start font-poppin;">Tgl Bln Thn</div>
-            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="/Pengcpns" style="color:rgba(2, 83, 107, 0.811)">Pengadaan CPNS 2022</a></div><br>
-            <div class="text-start font-poppin">Tgl Bln Thn</div>
-            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="/Pengcpns" style="color:rgba(2, 83, 107, 0.811)">Pengadaan CPNS 2022</a></div><br>
-            <div class="text-start font-poppin">Tgl Bln Thn</div>
-            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="/Pengcpns" style="color:rgba(2, 83, 107, 0.811)">Pengadaan CPNS 2022</a></div><br>
+            @if ($infolain->count() <= 3)
+            @for ($p=0; $p<1;$p++)
+            <div class="text-start font-poppin">{{ $infolain[$p]->date }}</div>
+            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i>
+                <a href="/Bimtekadm" style="color:rgba(2, 83, 107, 0.811)">
+                    {{ $infolain[$p]->judulBerita }}
+                </a>
+            </div>
+            <br>
+             @endfor
+            @else
+            @for ($p=0; $p<3;$p++)
+            <div class="text-start font-poppin">{{ $infolain[$p]->date }}</div>
+            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i>
+                <a href="/Bimtekadm" style="color:rgba(2, 83, 107, 0.811)">
+                    {{ $infolain[$p]->judulBerita }}
+                </a>
+            </div>
+            <br>
+            @endfor
+            @endif
+
           </div>
-        </div> 
+        </div>
         <br>
       </div>
       <div class="col" style="background-color: rgba(2, 83, 107, 0.811);color: white);"><br>
         <span class="text-coulumn" style="margin-top: 12px; color: white;">EVENT</span>
         <hr class="mx-auto" width="40%">
-        <div class="position-relative"> 
+        <div class="position-relative">
           <div class="position-absolute top-0 start-18">
-            <div class="text-start font-poppin text-white">Tgl Bln Thn</div>
-            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="/Simpegsapk" style="color:white">Sosialisasi SIMPEG & SAPK 2022</a></div><br>
-            <div class="text-start font-poppin text-white">Tgl Bln Thn</div>
-            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="/Simpegsapk" style="color:white">Sosialisasi SIMPEG & SAPK 2022</a></div><br>
-            <div class="text-start font-poppin text-white">Tgl Bln Thn</div>
-            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="/Simpegsapk" style="color:white">Sosialisasi SIMPEG & SAPK 2022</a></div><br>
-            
-            
+            @if ($event->count() < '3')
+            @for ($p=0; $p<1;$p++)
+            <div class="text-start font-poppin" style="color: white">{{ $event[$p]->date }}</div>
+            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i>
+                <a href="/Bimtekadm" style="color:rgba(255, 255, 255, 0.811)">
+                    {{ $event[$p]->judulBerita }}
+                </a>
+            </div>
+            <br>
+             @endfor
+            @else
+            @for ($p=0; $p<3;$p++)
+            <div class="text-start font-poppin">{{ $event[$p]->date }}</div>
+            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i>
+                <a href="/Bimtekadm" style="color:rgba(255, 255, 255, 0.811)">
+                    {{ $event[$p]->judulBerita }}
+                </a>
+            </div>
+            <br>
+            @endfor
+            @endif
+
           </div>
           <div class="text-center top-50" style="padding-top: 50%;">
             <a href="/Btnlihse" class="btn btn-light mb-4 mt-2 justify-content-center" style="border-radius: 30px">
@@ -39,18 +69,40 @@
       </div>
       <div class="col" style="background-color: rgba(226, 223, 223, 0.825);color: black;"><br>
         <span class="text-coulumn" style="margin-top: 12px">PENGUMUMAN</span>
- 
           <div class="position-relative">
             <div style="">
               <hr class="mx-auto" width="40%">
               <div style="padding-left: 70px">
-                <div class="text-start font-poppin">Tgl Bln Thn</div>
-                <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="/Bimtekadm" style="color:rgba(2, 83, 107, 0.811)">Bimbingan Teknis Pengelola Adm</a></div><br>
-                <div class="text-start font-poppin">Tgl Bln Thn</div>
-                <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="/Bimtekadm" style="color:rgba(2, 83, 107, 0.811)">Bimbingan Teknis Pengelola Adm</a></div><br>
-                <div class="text-start font-poppin">Tgl Bln Thn</div>
-                <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i><a href="/Bimtekadm" style="color:rgba(2, 83, 107, 0.811)">Bimbingan Teknis Pengelola Adm</a></div><br>
-                <br> 
+                {{-- @for ($p=0; $p<3;$p++)
+                <div class="text-start font-poppin">{{ $pengumuman[$p]->date }}</div>
+                <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i>
+                    <a href="/Bimtekadm" style="color:rgba(2, 83, 107, 0.811)">
+                        {{ $pengumuman[$p]->judulBerita }}
+                    </a>
+                </div>
+                <br>
+                @endfor --}}
+                @if ($pengumuman->count() < '3')
+            @for ($p=0; $p<1;$p++)
+            <div class="text-start font-poppin" style="color: white">{{ $event[$p]->date }}</div>
+            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i>
+                <a href="/Bimtekadm" style="color:rgba(2, 83, 107, 0.811">
+                    {{ $pengumuman[$p]->judulBerita }}
+                </a>
+            </div>
+            <br>
+             @endfor
+            @else
+            @for ($p=0; $p<3;$p++)
+            <div class="text-start font-poppin">{{ $pengumuman[$p]->date }}</div>
+            <div class="text-start font-poppin" style="font-size: 20px" <i class="fa fa-text-width" aria-hidden="true"></i>
+                <a href="/Bimtekadm" style="color:rgba(2, 83, 107, 0.811)">
+                    {{ $pengumuman[$p]->judulBerita }}
+                </a>
+            </div>
+            <br>
+            @endfor
+            @endif
               </div>
             </div>
           </div>
